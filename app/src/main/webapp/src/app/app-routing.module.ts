dcdcf224e0a792dc960412944ef179c326c62f9a
Oliver Eckle <iee1394@gmx.de>
2023-01-29 02:32:17 +0100

    working on refactoring

--- app/src/main/webapp/src/app/app-routing.module.ts
+++ app/src/main/webapp/src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
   {path: 'backoffice', loadChildren: () => import('./backoffice/backoffice.module').then(m => m.BackofficeModule), canActivate: [AuthGuard]},
-  {path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule), canActivate: [AuthGuard]},
 ];
 
 @NgModule({
